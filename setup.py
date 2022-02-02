d92dfcdc9b775bc448c3277eb15d8e05e6edf9cb
Matthew Mangione <matthewmmangione@gmail.com>
2022-02-02 08:44:08 -0500

    add dist after build

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+from gettext import install
+from platform import python_revision
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='try-pipenv',
+    name='pipenv-lambda',
     version='0.1.0',
     author_email='matthewmmangione@gmail.com',
-    description='try out pipenv and create a jsnon file',
+    description='try out pipenv to create a lambda',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6'
 )
